c6f1920deccf319fe86d3ae582dc9186f3905186
yen-cheng <lin0905580802@gmail.com>
2025-01-17 11:11:52 +0800

    feat: add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,11 +7,42 @@ import "./globals.css";
 const genSenB = localFont({
     src: "./fonts/GenSenRounded2TW-B.otf",
     variable: "--font-gensenb",
+    preload: true,
 });
 
 export const metadata: Metadata = {
     title: "Yen Cheng Lin",
     description: "The personal website of Yen Cheng Lin.",
+    keywords: ["Yen Cheng Lin", "Yen Cheng", "Ridemountainpig", "林彥成"],
+    authors: [
+        {
+            name: "ridemountainpig",
+            url: "https://www.github.com/ridemountainpig",
+        },
+    ],
+    openGraph: {
+        type: "website",
+        url: "https://yencheng.dev/",
+        title: "Yen Cheng Lin",
+        description:
+            "The personal website of Yen Cheng Lin, a software engineer and coffee enthusiast.",
+        images: [
+            {
+                url: "https://yencheng.dev/favicon.ico",
+                width: 1200,
+                height: 630,
+                alt: "Yen Cheng Lin",
+            },
+        ],
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "Yen Cheng Lin",
+        description:
+            "The personal website of Yen Cheng Lin, a software engineer and coffee enthusiast.",
+        creator: "@ridemountainpig",
+        images: ["https://yencheng.dev/favicon.ico"],
+    },
 };
 
 export default function RootLayout({
